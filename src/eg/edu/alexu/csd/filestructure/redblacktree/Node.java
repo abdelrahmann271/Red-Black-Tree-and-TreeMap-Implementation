5b03193214dd4e9beac7a32721422aae99f22b6c
Abdelrahman Nour <abdelrahmann271@gmail.com>
2020-03-17 23:51:39 +0200

    First Commit

---
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/Node.java
@@ -0,0 +1,83 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+public class Node<T extends Comparable<T>, V> implements INode<T, V> {
+
+	@Override
+	public void setParent(INode<T, V> parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode<T, V> getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLeftChild(INode<T, V> leftChild) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode<T, V> getLeftChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRightChild(INode<T, V> rightChild) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode<T, V> getRightChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setKey(T key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setValue(V value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getColor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setColor(boolean color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isNull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/RedBlackTree.java
@@ -0,0 +1,47 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T, V>  {
+
+	@Override
+	public INode<T, V> getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V search(T key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void insert(T key, V value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean delete(T key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
